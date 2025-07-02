77e6006176b250c40fea942f4e42496ae5629b5d
minhto2802 <tnnhatminh@gmail.com>
2025-07-02 14:17:33 -0700

    dpe package in progress

--- tests/demo.py
+++ tests/demo.py
@@ -1,13 +1,14 @@
 import sys
-
-from numba.tests.test_random import numpy_seed
-
 sys.path.append('..')
 
 from src.dpe import DPE
+from src.dpe.misc import fix_random_seed
 
 
 def main():
+    SEED = 0
+    fix_random_seed(SEED)
+
     dpe = DPE(
         data_dir=r'/scratch/ssd004/scratch/minht/checkpoints/sd0/Waterbirds/13574640',
         metadata_path='/scratch/ssd004/scratch/minht/datasets/waterbirds/metadata_waterbirds.csv',
@@ -17,8 +18,6 @@ def main():
         device='cuda',
     )
     dpe.fit()
-    print('Done')
-
 
 if __name__ == '__main__':
     main()
